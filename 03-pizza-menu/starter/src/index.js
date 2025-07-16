db1a1c2f68deb31b4b0dd274ccf78763e9ade7da
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-07-16 09:29:44 +0300

    create and reuse component

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { pizzaData } from "./data";
 
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Spinaci Pizza" />
+      <h2>{pizzaData[0].name}</h2>
+      <p>{pizzaData[0].ingredients}</p>
     </div>
   );
 }
