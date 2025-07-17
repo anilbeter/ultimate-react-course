296ed6626785430d8f48b4ab699c8c8092240f1b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-07-17 07:16:44 +0300

    Conditional Rendering With &&

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -22,24 +22,29 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza
-            // key={pizza.name}
-            // name={pizza.name}
-            // ingredients={pizza.ingredients}
-            // price={pizza.price}
-            // photoName={pizza.photoName}
-            // soldOut={pizza.soldOut}
-            pizza={pizza}
-            key={pizza.name}
-          />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza
+              // key={pizza.name}
+              // name={pizza.name}
+              // ingredients={pizza.ingredients}
+              // price={pizza.price}
+              // photoName={pizza.photoName}
+              // soldOut={pizza.soldOut}
+              pizza={pizza}
+              key={pizza.name}
+            />
+          ))}
+        </ul>
+      )}
 
       {/* <Pizza
         name="Pizza Spinaci"
@@ -66,7 +71,7 @@ function Pizza({ pizza }) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 7;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -77,7 +82,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
